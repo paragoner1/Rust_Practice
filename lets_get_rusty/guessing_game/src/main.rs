b661840cdb0163eb4923127b4d423cb2e00e176d
paragoner1 <shears_mete8f@icloud.com>
2025-03-23 13:58:03 -0500

    Hide secret number in guessing_game - Let's Get Rusty Ch 2

--- lets_get_rusty/guessing_game/src/main.rs
+++ lets_get_rusty/guessing_game/src/main.rs
@@ -1,17 +1,38 @@
-
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*;
 fn main() {
     println!("Guess the number!");
-    println
-    std::io::std
 
-    println!("Please input your guess."); 
+    let secret_number = rand::rng().random_range(1..=100);
 
-    let mut guess: String = String::new()
+    loop {
+    println!("Please input your guess.");
 
-    io::stdin(): stdin
-        .read_line(buf: &mut guess): Result<usize, Error>
-        .expect(msg: "Failed to read line");
-
-    println!("You guessed: {}", guess);
+    let mut guess = String::new();
     
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+            
+        };
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("{}", "Too Small!".red()),
+        Ordering::Greater => println!("{}", "Too Big!".red()),
+        Ordering::Equal => {
+            println!("{}", "You Win!".green());
+        break;
+    }
+}
+    }
 }
